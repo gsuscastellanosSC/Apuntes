9f6370411f734a561fcf2e4eec871cffb077aa6c
SC <jesuscastellanospaez@hotmail.com>
2019-10-09 22:05:02 -0500

    Curso de js Professional terminado

---
+++ javascript-profesional.js
@@ -0,0 +1,20 @@
+/*javascript-profesional*/
+	/*Cómo llega un script al navegador*/
+		DOM => "Estructua de arbol la terminar de procesarlo se tiene el"=> DOMContedLoaded(https://developer.mozilla.org/es/docs/Web/Events/DOMContentLoaded)
+
+		script async => "Se contesta primero el script mas pequeño"
+
+		script => <script>Siempre antes de cerrar el body, para que el script pueda encontrar todos los elementos</script>
+
+		src => eL DOM detine todo antes hasta qe se ejecute el SCRIPT
+
+		script defer src=> "La ejecución siempre ocure al final"
+
+	/*Cómo llega un script al navegador*/
+
+	/*Scope*/
+
+		type="module" => Limita el scope al archivo
+
+	/*Scope*/
+/*javascript-profesional*/
