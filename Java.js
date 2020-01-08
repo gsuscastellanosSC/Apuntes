5f3d1ed92e9f3fe85ca2f15f211ca58a0ca3d5eb
SC <jesuscastellanospaez@hotmail.com>
2020-01-08 16:50:36 -0500

    Update Apuntes of Java

---
+++ Java.js
@@ -0,0 +1,22 @@
+/*Tipos de colecciones*/
+  /*set*/
+    --> Permite almacenar elementos no repetidos y sin ordenar.
+    --> Acceso limitado.
+    --> Es muy rapida.
+  /*set*/
+
+  /*List*/
+    --> Permite elementos repetidos.
+    --> Los elementos estan indexados por un numero entero.
+    --> COnsume mas recursos que Set
+    --> Es implementada por ArrayList, LinkedList, Vector y poco usada CopyOnWriteArrayList
+  /*List*/
+
+  /*Queue*/
+  --> ES como una fila de banco.
+  /*Queue*/
+
+    /*Map*/
+      -->Identificador con su propia clave.
+    /*Map*/
+/*Tipos de colecciones*/
